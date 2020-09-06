6019d0696dde60b2ec9d4c9933eae002c5033685
BradD <bradleyeday@gmail.com>
2020-09-06 12:39:04 -0500

    password hashing

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPW) {
+    return bcrypt.compareSync(loginPW, this.password);
+}
+}
 
 User.init(
     {
